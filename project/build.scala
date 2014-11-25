cb0a3018264ca410cc7bcd30b3d151e3df7b57cc
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-11-25 19:17:13 +0100

    COnfiguration in which everything works except
    the scalahost call

--- project/build.scala
+++ project/build.scala
@@ -1,13 +1,29 @@
 import sbt._
 import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+import complete.DefaultParsers._
 
 object build extends Build {
-
-  lazy val commonSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.11.2")
+  import Dependencies._
+  import Settings._
+  
+  lazy val commonDependencies = Seq(
+    libraryDependencies <++= (scalaVersion)(sv => Seq(
+      Dependencies.tql)))
 
   lazy val core = Project(
     id = "core",
     base = file("core"),
-    settings = commonSettings)
+    settings = sharedSettings ++ commonDependencies ++ Seq(
+    scalacOptions ++= Seq(
+      "-Xplugin:"+Dependencies.obey_plugin,
+      "-P:obey:addRules:rules/target/scala-2.11/classes"))) dependsOn(rules)
+
+  lazy val rules = Project(
+    id = "rules",
+    base = file("rules"),
+    settings = sharedSettings ++ commonDependencies ++ List(
+      libraryDependencies ++= Seq(Dependencies.scalatest)))
+
 }
