9119c6e6a09129b9e0bab77ce768ac47853cf9c2
Adrien Ghosn <ghosn.adrien@gmail.com>
2015-01-14 17:41:23 +0100

    Urffff

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,6 @@ object Dependencies {
 	
 	def reflect(sv: String) = "org.scala-lang" % "scala-reflect" % sv
   def compiler(sv: String) = "org.scala-lang" % "scala-compiler" % sv
-  lazy val paradise ="org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full
   
   lazy val tql = "com.github.begeric" % "tqlscalameta_2.11" % "0.1-SNAPSHOT"
   
